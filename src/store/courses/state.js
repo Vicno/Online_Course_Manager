ceca9d18b0c8f9e270e784d47cca8967b0d50853
Vicno <diegorosazza@gmail.com>
2023-06-04 22:24:33 -0400

    Added content to the courses vuex store, algo created and empty students module for vuex

---
+++ src/store/courses/state.js
@@ -0,0 +1,60 @@
+export default function () {
+  return {
+    courses: [
+      {
+        name: "Java Advanced",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in...",
+        score: 4.5,
+        duration: "3:20",
+        tag: "Java",
+        imageSrc: require("../../assets/javaLogo.png"),
+      },
+      {
+        name: "JavaScript Basic",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in...",
+        score: 4.5,
+        duration: "3:20",
+        tag: "JavaScript",
+        imageSrc: require("../../assets/javascriptLogo.png"),
+      },
+      {
+        name: "Python",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in...",
+        score: 4.5,
+        duration: "3:20",
+        tag: "Python",
+        imageSrc: require("../../assets/pythonLogo.png"),
+      },
+      {
+        name: "UI/UX Design",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in...",
+        score: 4.5,
+        duration: "3:20",
+        tag: "UI/UX",
+        imageSrc: require("../../assets/UXUI.png"),
+      },
+      {
+        name: "Spring Boot",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in...",
+        score: 4.5,
+        duration: "3:20",
+        tag: "Spring Boot",
+        imageSrc: require("../../assets/springBootLogo.png"),
+      },
+      {
+        name: "Vue.js Advanced",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in...",
+        score: 4.5,
+        duration: "3:20",
+        tag: "Vue.js",
+        imageSrc: require("../../assets/vueLogo.svg"),
+      },
+    ],
+  };
+}
